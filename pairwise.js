3e676e48f4098cdbc3022cc5b07ec5684768ff32
Nate Mallison <healthynate@gmail.com>
2018-03-25 20:36:55 -0400

    updated solution

--- pairwise.js
+++ pairwise.js
@@ -13,51 +13,64 @@
 // 3 + 3 = 6 → Return 6
 
 
-function pairwise(arr, arg) {
-  // if arr length is zero answer will either be 0 if elements match or 0 if not, no non-found result answer specified
-  if (arr.length === 1) {
-    return 0;
-  }
-  // walk through the array
-  return arr.reduce((result, num, index) => {
-    // set repeat flag
-    let repeat = false;
-    // check this element against every following element
-    for (let i = index + 1; i < arr.length; i++) {
-      // if a match is found for the sum of arg
-      if (num + arr[i] === arg) {
-        // walk through the existing result pairs
-        for (let j = 0; j < result.length - 1; j++) {
-          // if the first element found already matches an existing first element, or if the second element found matches an existing second element
-          if ((result[j][0] === num && result[j][1] === index) || (result[j + 1][0] === arr[i] && result[j + 1][1] === i)) {
-            // set repeat to true and break this loop
-            repeat = true;
-            break;
-          }
-          // if the first element found matches second element already found or if the second element found already matches first element found
-          if ((result[j][0] === arr[i] && result[j][1] === i) || (result[j + 1][0] === num && result[j + 1][1] === index)) {
-            // set repeat to true and break this loop            
-            repeat = true;
-            break;
-          }
-        }
-        if (!repeat) {
-          // if we have not found a repeat, push the found match onto the result array
-          result.push([num, index], [arr[i], i]);
-          return result;
-        } else {
-          // we have found a repeat, reset repeat flag and continue our search
-          repeat = false;
-          continue;
-        }
-      } 
+// function pairwise(arr, arg) {
+//   // if arr length is zero answer will either be 0 if elements match or 0 if not, no non-found result answer specified
+//   if (arr.length === 1) {
+//     return 0;
+//   }
+//   // walk through the array
+//   return arr.reduce((result, num, index) => {
+//     // set repeat flag
+//     let repeat = false;
+//     // check this element against every following element
+//     for (let i = index + 1; i < arr.length; i++) {
+//       // if a match is found for the sum of arg
+//       if (num + arr[i] === arg) {
+//         // walk through the existing result pairs
+//         for (let j = 0; j < result.length - 1; j++) {
+//           // if the first element found already matches an existing first element, or if the second element found matches an existing second element
+//           if ((result[j][0] === num && result[j][1] === index) || (result[j + 1][0] === arr[i] && result[j + 1][1] === i)) {
+//             // set repeat to true and break this loop
+//             repeat = true;
+//             break;
+//           }
+//           // if the first element found matches second element already found or if the second element found already matches first element found
+//           if ((result[j][0] === arr[i] && result[j][1] === i) || (result[j + 1][0] === num && result[j + 1][1] === index)) {
+//             // set repeat to true and break this loop            
+//             repeat = true;
+//             break;
+//           }
+//         }
+//         if (!repeat) {
+//           // if we have not found a repeat, push the found match onto the result array
+//           result.push([num, index], [arr[i], i]);
+//           return result;
+//         } else {
+//           // we have found a repeat, reset repeat flag and continue our search
+//           repeat = false;
+//           continue;
+//         }
+//       } 
+//     }
+//     return result;
+//   }, []).reduce((result, foundPair) => {
+//     // take the final array of pairs and sum the indexes
+//     result += foundPair[1];
+//     return result;
+//   }, 0);
+// }
+
+function pairwise(arr, arg){
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] + arr[j] === arg) {
+        sum += i + j;
+        arr[i] = arr[j] = NaN;
+      }
     }
-    return result;
-  }, []).reduce((result, foundPair) => {
-    // take the final array of pairs and sum the indexes
-    result += foundPair[1];
-    return result;
-  }, 0);
+  }
+  return sum;
 }
 
 console.log(pairwise([1,4,2,3,0,5], 7));
